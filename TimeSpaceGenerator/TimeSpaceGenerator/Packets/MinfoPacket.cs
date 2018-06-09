cf47553d5cf4b73c27094e4a4b499f3bcac64eb8
Elendan <arthur.despeaux@hotmail.fr>
2018-06-09 19:34:01 +0200

    fix minfo packet

--- TimeSpaceGenerator/TimeSpaceGenerator/Packets/MinfoPacket.cs
+++ TimeSpaceGenerator/TimeSpaceGenerator/Packets/MinfoPacket.cs
@@ -32,9 +32,21 @@ namespace TimeSpaceGenerator.Packets
         public string Unknown7 { get; set; }
 
         [PacketIndex(7)]
-        public short Lives { get; set; }
+        public short Unknown8 { get; set; }
 
         [PacketIndex(8)]
-        public string Unknown8 { get; set; }
+        public string Unknown9 { get; set; }
+
+        [PacketIndex(9)]
+        public string Unknown10 { get; set; }
+
+        [PacketIndex(10)]
+        public byte Lives { get; set; }
+
+        [PacketIndex(11)]
+        public string Unknown11 { get; set; }
+
+        [PacketIndex(12)]
+        public string Unknown12 { get; set; }
     }
 }
